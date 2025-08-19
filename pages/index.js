c194b1c4be36aed21b1149aca89cada6cc5a4d37
rancis <santosrancis04@gmail.com>
2025-08-19 14:48:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I am Rancis Santos. 20 years old 3rd Year Student from the University of the Assumption currently taking the Bachelor of Science in Information Technology. I am intereseted in the matters of geopraphy and global demographics. I love watching movies and documentaries about historyand wars. I want to be a successful after graduating by pursuing software engineering and earning money. </p>
+        <p>Hello I am Rancis Santos. 20 years old 3rd Year Student from the University of the Assumption currently taking the Bachelor of Science in Information Technology. I am intereseted in the matters of geogpraphy and global demographics. I love watching movies and documentaries about history and wars. I see myself five years from now as a successful after graduating by pursuing software engineering and earning money. </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
